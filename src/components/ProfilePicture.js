73add3e7772d631597f35eefef971fd4278c1e58
Raymond Wolt <rjwolt@gmail.com>
2021-10-06 19:00:11 -0500

    Add file input for profile picture preview

--- src/components/ProfilePicture.js
+++ src/components/ProfilePicture.js
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, {Component} from 'react';
 import defaultProfilePicture from '../images/profile.png';
 import '../styles/ProfilePicture.css';
 
-function ProfilePicture() {
-    return(
-        <div className="profile-container">
-            <img className='profile-picture' src={defaultProfilePicture} alt="Default Profile" />
-            <p className="profile-text">Edit</p>
-        </div>
-    )
+class ProfilePicture extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    handleChange = (e) => {
+        const files = e.target.files;
+        console.log(files);
+        const url = window.URL.createObjectURL(files[0]);
+        console.log(url);
+        document.querySelector('.profile-picture').src = url;
+    }
+
+
+    render() {
+        return(
+            <div className="profile-container">
+                <input type='file'
+                    id="image-upload"
+                    onChange={this.handleChange}
+                />
+                <img className="profile-picture"
+                    onClick={() => document.getElementById('image-upload').click()}
+                    src={defaultProfilePicture} alt="Default Profile"
+                />
+                <p className="profile-text">Edit</p>
+            </div>
+        )
+    }
 }
 
 export default ProfilePicture;
